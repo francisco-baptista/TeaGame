2543021d2571fbfd2ed85bbb8a4a6f402d0fc041
Rob Noake <rob.noake@clock.co.uk>
2011-11-25 15:01:03 +0000

    Add spreadsheet key to config

--- Config.php
+++ Config.php
@@ -7,3 +7,4 @@
 // Google credentials
 define("GOOGLE_USERNAME","clockteagame@clock.co.uk");
 define("GOOGLE_PASSWORD","august swim sky v3");
+define("GOOGLE_KEY","0Aj7iW0x6w5ftdGN2ejVweXJPSkpHWkFvWnVXTEgtUVE");
