0c10380fc046bef0e1dc97afa31c85dbd9881049
Rob Noake <rob.noake@clock.co.uk>
2011-11-25 14:59:32 +0000

    Adding config file with gmail credentials

---
+++ Config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Configuration file for Clock tea game.
+ *
+ */
+
+// Google credentials
+define("GOOGLE_USERNAME","clockteagame@clock.co.uk");
+define("GOOGLE_PASSWORD","august swim sky v3");
