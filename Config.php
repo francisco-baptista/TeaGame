1c182aa099a8ceec40c1e99c2a20bc06593c8bdb
Francisco Baptista <francisco@tomboa.net>
2011-11-25 15:24:13 +0000

    fix include path Signed-off-by:Francisco Baptista <francisco@tomboa.net>

--- Config.php
+++ Config.php
@@ -5,6 +5,6 @@
  */
 
 // Google credentials
-define("GOOGLE_USERNAME","clockteagame@clock.co.uk");
+define("GOOGLE_USERNAME","clockteagame@gmail.com");
 define("GOOGLE_PASSWORD","01xAmBdcQqnIdzhmZM4t");
 define("GOOGLE_KEY","0Aj7iW0x6w5ftdGN2ejVweXJPSkpHWkFvWnVXTEgtUVE");
