dacfd5bca16ad532d5d88e890cef134ca8803e9b
Francisco Baptista <francisco@tomboa.net>
2011-11-25 15:15:28 +0000

    fix include path Signed-off-by:Francisco Baptista <francisco@tomboa.net>

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  */
 
 // Zend library include path
-set_include_path(get_include_path() . PATH_SEPARATOR . "$_SERVER[DOCUMENT_ROOT]/ZendGdata-1.11.11/library");
+set_include_path(get_include_path() . PATH_SEPARATOR . $_SERVER['DOCUMENT_ROOT'] . "/ClockTeaGame/ZendGdata-1.11.11/library");
  
 include_once("Google_Spreadsheet.php");
 
